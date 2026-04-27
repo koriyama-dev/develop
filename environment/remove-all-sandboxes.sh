d02123ed2e6bdc63c7f0dee8ead35e0cea545905
koriyama <kuni.k.5092@gmail.com>
2026-04-27 14:42:52 +0900

    修正

--- environment/remove-all-sandboxes.sh
+++ environment/remove-all-sandboxes.sh
@@ -2,7 +2,7 @@
 
 # 全sandboxを削除
 echo "Removing all sandboxes..."
-for dir in /home/developer/develop/sandbox*/; do
+for dir in /home/developer/develop/environment/sandbox*/; do
   if [ -f "$dir/docker-compose.yaml" ]; then
     cd "$dir" && docker compose down
   fi
--- environment/switch-sandbox.sh
+++ environment/switch-sandbox.sh
@@ -8,19 +8,19 @@ fi
 
 # 全sandboxを削除
 echo "Removing all sandboxes..."
-for dir in /home/developer/develop/sandbox*/; do
+for dir in /home/developer/develop/environment/sandbox*/; do
   if [ -f "$dir/docker-compose.yaml" ]; then
     cd "$dir" && docker compose down
   fi
 done
 
 # devcontainer.jsonを書き換え
-sed -i "s|../../[^/]*/docker-compose.yaml|../../${SANDBOX}/docker-compose.yaml|" /home/developer/develop/src/.devcontainer/devcontainer.json
+sed -i "s|../../[^/]*/docker-compose.yaml|../../${SANDBOX}/docker-compose.yaml|" /home/developer/develop/environment/src/.devcontainer/devcontainer.json
 echo "Switched to ${SANDBOX}"
 
 # 新しいsandboxを起動
 echo "Starting $SANDBOX..."
-cd /home/developer/develop/$SANDBOX && docker compose up -d --build
+cd /home/developer/develop/environment/$SANDBOX && docker compose up -d --build
 
 # VSCodeを起動
-export SANDBOX_NAME=$SANDBOX && code /home/developer/develop/src
+export SANDBOX_NAME=$SANDBOX && code /home/developer/develop/environment/src
