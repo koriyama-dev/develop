3e4fcedc453c630a8183e15494bae77c93bf9380
koriyama <kuni.k.5092@gmail.com>
2026-05-07 18:04:15 +0900

    修正

--- environment/switch-sandbox.sh
+++ environment/switch-sandbox.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SANDBOX=$1
-
 if [ -z "$SANDBOX" ]; then
   echo "Usage: switch-sandbox.sh <sandbox_name>"
   exit 1
@@ -21,6 +20,19 @@ echo "Switched to ${SANDBOX}"
 # 新しいsandboxを起動
 echo "Starting $SANDBOX..."
 cd /home/developer/develop/environment/$SANDBOX && docker compose up -d --build
+
+# vscode-server volumeの所有権を修正
+echo "Fixing volume permissions..."
+docker volume create ${SANDBOX}_devcontainer-vscode-server 2>/dev/null
+docker run --rm \
+  -v ${SANDBOX}_devcontainer-vscode-server:/var/www/.vscode-server \
+  busybox chown -R 1000:1000 /var/www/.vscode-server
+
+# cline-data volumeの所有権を修正
+docker volume create ${SANDBOX}_devcontainer-cline-data 2>/dev/null
+docker run --rm \
+  -v ${SANDBOX}_devcontainer-cline-data:/var/www/.cline \
+  busybox chown -R 1000:1000 /var/www/.cline
 
 # VSCodeを起動
 export SANDBOX_NAME=$SANDBOX && code /home/developer/develop/environment/src
